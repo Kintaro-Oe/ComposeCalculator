244ba65941941313c9f6f4b9834fdf7802673c8f
Kintaro-Oe <marcusclairmont@gmail.com>
2024-10-15 17:24:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Calculator
+
+### Create a calculator
+
+Create an app to the following specifications.  
+- A user can perform these actions:
+    - Clear - Reset current calculation
+    - Delete - Removes last typed char (number, operator or decimal)
+    - Calculate - Evaluate given equation 
+    - Insert number - Enter numbers char 0 - 9
+    - Insert decimal - Numbers may be decimals
+- A user can perform these operations: Add, Subtract, Multiply, Divide
+- A user can see the calculation result and perform further calculations on the result
+1080x2400
+
+## Demo
+
+<img src="img/Calculator.png" width="270" height="600" />
---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.calculator
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.ui.theme.MediumGrey
+
+class  MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGrey)
+                        .windowInsetsPadding(WindowInsets.navigationBars)
+                        .padding(16.dp)
+                )
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+        }
+    }
+}
